43b305cc848c8a73e9b0a3f8262f2af17638c990
Luiz Mineo <luiz.mineo@gmail.com>
2014-05-04 16:48:09 -0300

    port to bloodless

---
+++ server/server.dart
@@ -0,0 +1,24 @@
+import 'package:bloodless/server.dart' as app;
+import 'package:logging/logging.dart';
+
+var logger = new Logger("hello_world");
+
+@app.Route("/hello")
+helloWorld(@app.QueryParam() String name) {
+
+  if (name != null) {
+    logger.info("Saying hello to $name");
+    return "Hello $name";
+  } else {
+    logger.info("Saying hello to the world");
+    return "Hello World!";  
+  }
+
+}
+
+main() {
+
+  app.setupConsoleLog();
+  app.start();
+
+}
